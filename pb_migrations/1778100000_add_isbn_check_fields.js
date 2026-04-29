ea131098a9a5dc23b7e9019faff3fb998e5e3eb3
wesochuck <wosborn@clcohio.org>
2026-04-29 15:23:39 -0400

    Hide numeric workflow tags in staff title badges

---
+++ pb_migrations/1778100000_add_isbn_check_fields.js
@@ -0,0 +1,59 @@
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("title_requests");
+
+  collection.fields.add(new Field({
+    "system": false,
+    "id": "select_isbn_check_status",
+    "name": "isbnCheckStatus",
+    "type": "select",
+    "required": false,
+    "presentable": false,
+    "maxSelect": 1,
+    "values": ["pending", "found", "not_found", "error", "error_max_retries", "skipped_no_isbn", "found_in_polaris" ]
+  }));
+
+  collection.fields.add(new Field({
+    "system": false,
+    "id": "text_isbn_check_result",
+    "name": "isbnCheckResult",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "min": 0,
+    "max": 0,
+    "pattern": "",
+    "autogeneratePattern": ""
+  }));
+
+  collection.fields.add(new Field({
+    "system": false,
+    "id": "number_isbn_check_retry",
+    "name": "isbnCheckRetryCount",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "min": null,
+    "max": null,
+    "onlyInt": true
+  }));
+
+  collection.fields.add(new Field({
+    "system": false,
+    "id": "autodate_last_checked",
+    "name": "lastChecked",
+    "type": "autodate",
+    "required": false,
+    "presentable": false,
+    "onCreate": false,
+    "onUpdate": false
+  }));
+
+  app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("title_requests");
+  collection.fields.removeByName("isbnCheckStatus");
+  collection.fields.removeByName("isbnCheckResult");
+  collection.fields.removeByName("isbnCheckRetryCount");
+  collection.fields.removeByName("lastChecked");
+  app.save(collection);
+});
